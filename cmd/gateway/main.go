352e0824a374c2cb20dea72f242a4db2ce857b60
好好住-liyuanwu <liyuanwu@haohaozhu.com>
2023-04-10 19:03:21 +0800

    rebuild msgo

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -1,9 +1,31 @@
 package main
 
-import "github.com/liyuanwu2020/msgo"
+import (
+	"github.com/liyuanwu2020/msgo/engine"
+	"log"
+)
 
 func main() {
-	engine := msgo.Default()
+	e := engine.Default()
+	e.Get("/user", func(ctx *engine.Context) {
+		log.Println("hello")
+	})
+	e.Run(":80")
 
-	engine.Run(":80")
+	//engine := msgo.Default()
+	//engine.OpenGateWay = true
+	//var configs []gateway.GWConfig
+	//configs = append(configs, gateway.GWConfig{
+	//	Name: "order",
+	//	Path: "/order/**",
+	//	Host: "127.0.0.1",
+	//	Port: 9003,
+	//}, gateway.GWConfig{
+	//	Name: "goods",
+	//	Path: "/goods/**",
+	//	Host: "127.0.0.1",
+	//	Port: 9002,
+	//})
+	//engine.SetGateConfigs(configs)
+	//engine.Run(":80")
 }
